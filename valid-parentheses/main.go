172fbd361c8e56df0eab3b34b7a82cd6dee1ee8d
kilingzhang <i@kilingzhang.com>
2020-05-12 01:57:20 +0800

    :computer: 2020-05-12

--- valid-parentheses/main.go
+++ valid-parentheses/main.go
@@ -4,8 +4,30 @@ import (
 	tool "github.com/kilingzhang/leetcode"
 )
 
+func isValid(s string) bool {
 
+	if s == "" {
+		return true
+	}
 
+	stack := []uint8{s[0]}
+	for i := 1; i < len(s); i++ {
+		if len(stack) != 0 && stack[len(stack)-1] == '(' && s[i] == ')' {
+			stack = stack[:len(stack)-1]
+			continue
+		}
+		if len(stack) != 0 && stack[len(stack)-1] == '[' && s[i] == ']' {
+			stack = stack[:len(stack)-1]
+			continue
+		}
+		if len(stack) != 0 && stack[len(stack)-1] == '{' && s[i] == '}' {
+			stack = stack[:len(stack)-1]
+			continue
+		}
+		stack = append(stack, s[i])
+	}
+	return len(stack) == 0
+}
 
 func main() {
 	tool.Dump(isValid("()[]{}"))
